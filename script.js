a32c78dc9b4806c0f4d2b7a2f63f2766bce11096
Abhay <abhaycormourya@gmail.com>
2024-05-15 11:19:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 document.addEventListener('DOMContentLoaded', function () {
   // Fetch API data
-  fetch('https://random-api.xyz/api/fun/fortune')
+  fetch('https://random-api.xyz/api/fun/quote')
     .then(response => response.json())
     .then(data => {
       // Display data in the paragraph tag
       const apiDataElement = document.getElementById('apiData');
-      apiDataElement.textContent = JSON.stringify(data.fortune);
+      apiDataElement.textContent = JSON.stringify(data.quote);
       console.log(data);
     })
     .catch(error => {
